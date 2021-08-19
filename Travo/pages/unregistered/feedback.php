8add20cbc6fec9f50b9d344e4066b25a6ffb7a48
awanthairushan <avanthairushan@gmail.com>
2021-08-19 13:23:49 +0530

    repeatable about_us

--- Travo/pages/unregistered/feedback.php
+++ Travo/pages/unregistered/feedback.php
@@ -15,62 +15,78 @@
       <table class="feedback_table-feedback">
         <thead class="feedback_thead-feedback">
           <tr>
-            <td>DATE</td>
-            <td>FEEDBACK</td>
+            <td class="date-feedback">DATE</td>
+            <td class="feedback-feedback">FEEDBACK</td>
           </tr>
         </thead>
         <tbody class="feedback_tbody-feedback">
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td class="feedback-feedback">feedback</td>
+            <td class="feedback-feedback">feedback 1</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 2</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 3</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 4</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 5</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 6</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 7</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 8</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 9</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 10</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 11</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 12</td>
           </tr>
           <tr>
             <td class="date-feedback">YYYY-MM-DD</td>
-            <td>feedback 1</td>
+            <td class="feedback-feedback">feedback 13</td>
+          </tr>
+          <tr>
+            <td class="date-feedback">YYYY-MM-DD</td>
+            <td class="feedback-feedback">feedback 14</td>
+          </tr>
+          <tr>
+            <td class="date-feedback">YYYY-MM-DD</td>
+            <td class="feedback-feedback">feedback 15</td>
+          </tr>
+          <tr>
+            <td class="date-feedback">YYYY-MM-DD</td>
+            <td class="feedback-feedback">feedback 16</td>
+          </tr>
+          <tr>
+            <td class="date-feedback">YYYY-MM-DD</td>
+            <td class="feedback-feedback">feedback 17</td>
           </tr>
         </tbody>
       </table>
@@ -79,6 +95,6 @@
     <section id="contact_us-section">
       <?php include '../../repeatable_contents/footer.php';?>
       <style> <?php include '../../repeatable_contents/footer.css'; ?>  </style>
-        </section>
+    </section>
   </body>
 </html>
---
+++ Travo/repeatable_contents/about_us.php
@@ -0,0 +1,63 @@
+<?php
+  echo '
+  <section class="about_us-section" id="about_us-section">
+    <h1 class="h1-about_us">ABOUT US</h1>
+    <h3 class="h3-about_us">Just one click to explore the Taprobane...</h3>
+    <div class="div-about_us">
+      <p class="p-about_us">Travo.lk was recently launched in the travel management industry just for you who are interested in visiting Sri Lanka . We are a one-stop shop which is covering all your needs while providing a wonderful vacation as well as a safe journey for all domestic travellers. <br><br>Through our travel Management Service, we are committed to protecting the businesses and travelers who engage with us from savings, efficiency, and safety from Point Pedro to Devendra Point.<p>
+      <img class="logo-about_us" src="../../images/Logo/travo.png" alt="TRAVO.lk">
+    </div>
+    <hr class="hr-about_us">
+    <div onclick="prev()" class="prev_btn-about_us">
+       <img class="left_arrow-about_us" src="../../images/icons/left-arrow.png" alt="LEFT">
+    </div>
+    <div onclick="next()" class="next_btn-about_us">
+      <img class="right_arrow-about_us" src="../../images/icons/right-arrow.png" alt="RIGHT">
+    </div>
+    <div class="gallary-about_us" id="gallary">
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/1.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/2.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/3.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/4.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/5.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/6.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/7.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/8.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/9.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/10.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/11.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/12.jpg" alt="">
+      </div>
+      <div class="slide-about_us">
+        <img src="../../images/sample_images/for_homes/13.jpg" alt="">
+      </div>
+    </div>
+
+    <hr class="hr-about_us">
+
+  </section>
+  ';
+ ?>
